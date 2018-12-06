213782ae42dcdfa70abadc67e3440dd180c2158c
richie <kotzen1023@gmail.com>
2018-12-06 17:23:10 +0800

    modify

---
+++ app/src/main/java/com/seventhmoon/jamcast/ui/LocalBrowserFragment.java
@@ -0,0 +1,11 @@
+package com.seventhmoon.jamcast.ui;
+
+import android.support.v4.app.Fragment;
+
+import com.seventhmoon.jamcast.utils.LogHelper;
+
+public class LocalBrowserFragment extends Fragment {
+    private static final String TAG = LogHelper.makeLogTag(MediaBrowserFragment.class);
+
+
+}
